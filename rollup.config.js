c373ab1f8b3ea9d8d52833057c62d4ba4319df11
Bas Heerschop <heerschop@gmail.com>
2020-05-31 13:25:41 +0200

    Added @rollup/plugin-typescript

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,12 @@
+import typescript from '@rollup/plugin-typescript';
+
 export default {
-  input: 'tmp/main.js',
+  input: 'src/main.ts',
   treeshake: true,
   output: {
     file: 'dist/bundle.js',
     format: 'cjs',
     sourcemap: true
-  }
+  },
+  plugins: [typescript()]
 };
